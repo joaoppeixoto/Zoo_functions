7bc2d941902be84563f6062e010f1b580e0323a1
Joao Peixoto <joao.peixoto678@gmail.com>
2022-10-10 09:35:27 -0300

    test 3

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,8 +1,6 @@
 const data = require('../data/zoo_data');
-const { species, hours } = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  if (scheduleTarget === undefined) return hours;
 
 }
 module.exports = getSchedule;
