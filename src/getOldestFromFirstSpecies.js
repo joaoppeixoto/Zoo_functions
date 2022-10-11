d9e52355ff107a6afd870239736f94b7cb44c1ac
Joao Peixoto <joao.peixoto678@gmail.com>
2022-10-11 06:21:50 -0300

    mais um teste

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,9 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-
+  const animal = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const obj = species.find((specie) => specie.id === animal).residents;
+  return Object.values(obj.sort((a1, a2) => a1.age - a2.age)[obj.length - 1]);
 }
 
 module.exports = getOldestFromFirstSpecies;
