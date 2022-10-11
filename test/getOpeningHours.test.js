5cf3fa6d47604643f76535bb9794be9ada0e5a63
Joao Peixoto <joao.peixoto678@gmail.com>
2022-10-11 09:31:04 -0300

    mais uma coberturinha

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,26 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('se a função não receber parametros, retorna um objeto com todos os horários', () => {
+    expect(getOpeningHours('')).toStrictEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+
+  it('retorna se o zoo está fechado segunda', () => {
+    expect(getOpeningHours('monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+  it('retorna se o zoo está aberto', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+
+  it('retorna se o zoo está fechado quarta', () => {
+    expect(getOpeningHours('Wednesday', '07:00-AM')).toBe('The zoo is closed');
+  });
+});
