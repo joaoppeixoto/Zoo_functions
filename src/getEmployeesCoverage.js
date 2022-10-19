5711e7eb2d56dcfa59b24bdcd2798fa24cdaff34
Joao Peixoto <joao.peixoto678@gmail.com>
2022-10-19 10:41:16 -0300

    finally

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,32 +2,26 @@ const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getEmployer = (employer) => {
-  let verify;
-  if (!employer) verify = employees;
-  else {
-    verify = employees.filter((emp) => emp.firstName === employer.name
+  let verify = employees;
+  if (employer === undefined) return verify;
+  if (employees === 0) throw new Error('Informações inválidas');
+  verify = employees.filter((emp) => emp.firstName === employer.name
      || emp.lastName === employer.name || emp.id === employer.id);
-  }
   return verify;
 };
-
 function getEmployeesCoverage(employer) {
   const verify = getEmployer(employer);
   verify.forEach((value, index) => {
-    const locations = [];
-    const speciesss = [];
+    const objAuxiliar = { species: [], locations: [] };
     value.responsibleFor.forEach((specieId) => {
       const specieFound = species.filter((specie) => specie.id === specieId)[0];
-      speciesss.push(specieFound.name);
-      locations.push(specieFound.location);
+      objAuxiliar.species.push(specieFound.name);
+      objAuxiliar.locations.push(specieFound.location);
     });
-    verify[index] = {
+    verify[index] = { ...objAuxiliar,
       fullName: `${value.firstName} ${value.lastName}`,
-      species: speciesss,
-      locations,
       id: value.id };
   });
-  if (!verify) throw new Error('Informações inválidas');
   return verify.length === 1 ? verify[0] : verify;
 }
 
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
+const { hours, species} = data;
 
+const weekDay = Object.keys(hours);
+const total = () => {
+  const objeto = weekDay.reduce((acc, elem) => {
+    acc[elem] = {
+      officeHour: `Open from ${hours[elem].open}am until ${hours[elem].close}pm`,
+      exhibition: species.filter((animal) => animal.availability.includes(elem)).map((a) => a.name),
+    };
+    return acc;
+  }, {});
+  objeto.Monday = {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  };
+  return objeto;
+};
+
+function getSchedule(scheduleTarget) {
+  if (scheduleTarget === undefined) {
+    return total();
+  }
+  const verifica = species
+    .some((cond) => scheduleTarget === cond.name);
+  if (!verifica && !weekDay.includes(scheduleTarget)) {
+    return total();
+  }
+  if (weekDay.includes(scheduleTarget)) {
+    return { [scheduleTarget]: total()[scheduleTarget] };
+  }
+  return species.find((teste) => teste.name === scheduleTarget).availability;
 }
 
 module.exports = getSchedule;
